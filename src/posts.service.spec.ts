e190f019d67f3cd67f93fabdefa21a9b87ea02e6
Konstantin Shvagerius <konstantinshvagerius@yandex.ru>
2026-09-07 21:11:09 +0300

    test4

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -14,17 +14,20 @@ describe('PostsService', () => {
 
   it('should add a new post', () => {
     const createdPost = postsService.create(post);
-
+    expect(createdPost).toBeDefined();
     expect(createdPost.id).toBe('2');
-    expect(createdPost.text).toBe('Mocked post');
+    expect(createdPost.text).toBe(post.text);
     expect(createdPost.date).toBeDefined();
+    expect(new Date(createdPost.date).toISOString()).toBe(createdPost.date);
+    expect(postsService.find('2')).toEqual(createdPost);
+    expect(postsService.find('1')).toBeDefined();
   });
 
   it('should find a post', () => {
-    const createdPost = postsService.create(post);
-
-    const foundPost = postsService.find(createdPost.id);
-
-    expect(foundPost).toEqual(createdPost);
+    const existingPost = postsService.find('1');
+    expect(existingPost).toBeDefined();
+    expect(existingPost!.id).toBe('1');
+    expect(existingPost!.text).toBe('Some pre-existing post');
+    expect(postsService.find('non-existing-id')).toBeUndefined();
   });
 });
